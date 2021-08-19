27bc7f9032c19f27ad4f30cc1e4cf0cbf68e45ec
KK <kentech.1x1x3@gmail.com>
2021-08-19 13:31:36 +0000

    revised correct_user

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -26,12 +26,9 @@ class TasksController < ApplicationController
   end
 
   def edit
-      @task = Task.find(params[:id])
   end
 
   def update
-      @task = Task.find(params[:id])
-
     if @task.update(task_params)
       flash[:success] = 'Task は正常に更新されました'
       redirect_to @task
@@ -58,7 +55,7 @@ class TasksController < ApplicationController
   def correct_user
     @task = current_user.tasks.find_by(id: params[:id])
     unless @task
-      redirect_to login_url
+      redirect_to root_url
     end
   end    
     
